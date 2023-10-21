57794e67e563522fdb167be47fcf135ae705b026
Hunain <272002abhishek@gmail.com>
2023-10-22 00:49:20 +0530

    1st commit

---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+var date = new Date();
+function Footer(){
+    return <footer> <p>Copyright {date.getFullYear}</p></footer>;
+}
+export default Footer;
---
+++ src/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note(){
+    return <note>
+      <div className="note">
+        <h1>Note Title</h1>
+    <p>body</p>
+      </div>
+    
+    
+ 
+    </note>;
+}
+export default Note;
