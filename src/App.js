f2bd5eb4cd0db204f2a8808f5f6d1d996c607005
Hunain <272002abhishek@gmail.com>
2023-11-21 09:43:39 +0530

    mapping data into components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note"
-function App(){
-  return <div>
-    <Header/>
-    <Note/>
-    <Footer/>
-    </div>;
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./Note";
+import notes from "./notes";
+
+function createNotes(note) {
+  return <Note key={note.key} title={note.title} content={note.content} />;
 }
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(createNotes)}
+      
+      <Note title={notes[0].title} content={notes[0].content}/>
+      <Footer />
+    </div>
+  );
+}
+
 export default App;
